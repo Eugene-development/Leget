c701a1a4d4ccfc6719dacc6fffa02298ae71dd9d
Eugene Che <indexpro24@gmail.com>
2024-01-16 09:11:33 +0300

    c

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -145,18 +145,17 @@ function NavigationItem({ href, children }) {
 }
 function NoLinkNavigationItem({ href, children }) {
   return (
-      <p
-      className="group relative isolate -mx-6 bg-neutral-900 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
+      <p 
+      className="-mx-6 px-6 py-10  sm:mx-0 sm:px-0 sm:py-16 text-6xl  sm:even:mt-0 sm:even:border-l sm:even:border-neutral-900 sm:even:pl-16"
     >
       {children}
-      <span className="absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
+      <span className="absolute inset-y-0 -z-10 w-full bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
     </p>
   )
 }
 
 function Navigation() {
-    const pathname = usePathname()
-  console.log(pathname)
+  const pathname = usePathname()
 
   return (
     <nav className="mt-px font-display text-5xl font-medium tracking-tight text-white">
