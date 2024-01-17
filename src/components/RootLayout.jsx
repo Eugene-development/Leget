919dd6acafd7e7e0d9046a5d2b5f9f801b862adc
Eugene Che <indexpro24@gmail.com>
2024-01-17 20:02:24 +0300

    с

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -146,7 +146,7 @@ function NavigationItem({ href, children }) {
 function NoLinkNavigationItem({ href, children }) {
   return (
       <p 
-      className="-mx-6 px-6 py-10 sm:mx-0 sm:px-0 sm:py-16 text-6xl font-extrabold sm:even:mt-0 sm:even:border-l sm:even:border-neutral-900 sm:even:pl-16"
+      className="tracking-wide -mx-6 px-6 py-10 sm:mx-0 sm:px-0 sm:py-16 text-6xl font-extrabold sm:even:mt-0 sm:even:border-l sm:even:border-neutral-900 sm:even:pl-16"
     >
       {children}
       <span className="absolute inset-y-0 -z-10 w-full bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
